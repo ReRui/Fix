abcda616f0c39c44b442de26bd9393ad633a21a7
linhuarui1995 <764058804lhr@gmail.com>
2018-01-16 21:03:57 +0800

    acquire

---
+++ heu_search/src/main/fix/run/AcquireVariable.java
@@ -0,0 +1,29 @@
+
+package fix.run;
+
+import gov.nasa.jpf.Config;
+import gov.nasa.jpf.JPF;
+import fix.listener.LockListener;
+
+import java.util.Vector;
+
+public class AcquireVariable {
+    private static String filePath = "C:\\Users\\lhr\\Desktop\\lock.txt";
+    private static String fieldName = "amount";//要寻找的参数名
+    public static void main(String[] args) {
+        String[] str = new String[]{
+                "+classpath=out/production/heu_search",
+                "+search.class=fix.search.SingleExecutionSearch",
+                "account.Main"
+        };
+        Config config = new Config(str);
+        JPF jpf = new JPF(config);
+        LockListener lockListener = new LockListener(filePath,fieldName);
+        jpf.addListener(lockListener);
+        jpf.run();
+        Vector<String> oneLockfieldVector = lockListener.getOneLockfieldVector();
+        for(String s : oneLockfieldVector)
+            System.out.println("变量:" + s);
+    }
+
+}
